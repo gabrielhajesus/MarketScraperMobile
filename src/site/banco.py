fa235537f8211d383149cef14da6994b1b78dc61
Gabreiel Henrique <gabriel207983213@hotmail.com>
2023-06-21 16:05:08 -0300

    Projeto de scraper da Kabum

---
+++ src/site/banco.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+import pymongo
+
+class Banco:
+    def inicia_banco():
+        client = MongoClient('localhost', 27017)
+        db = client.Market_Scraper_Mobile
+        return db
+    
+    def kabum(db):
+        kabum = db.kabum
+        return kabum
+    
+    def pichau(db):
+        pichau = db.pichau
+        return pichau
+
+    def produtos(db):
+        produtos = db.produtos
+        return produtos
+     
+    def convertenome(nome):
+        nome = nome.replace(',','').replace('-','').replace(' ','').replace('(', '').replace(':','').replace('|','').replace('?','').replace('{','').replace('}','').replace('`','').replace('^', '').replace(')','').replace('.','').replace('/','').replace('"','').replace('!','').replace(';','').replace('+','').replace('~','').replace('<','').replace('>','').replace('	','').replace('*','').replace('\'','').lower()
+        return nome
