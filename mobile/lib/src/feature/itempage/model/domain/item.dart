e370ff9b3d730ae30fafa664d899780d87536597
Carlos Gusmão <carlos_9l6@hotmail.com>
2023-06-28 16:15:33 -0300

    .

--- mobile/lib/src/feature/itempage/model/domain/item.dart
+++ mobile/lib/src/feature/itempage/model/domain/item.dart
@@ -1,4 +1,3 @@
- 
  class Item {
   final String itemName;
   final double itemPrice;
--- mobile/lib/src/feature/itempage/view/page/itempage.dart
+++ mobile/lib/src/feature/itempage/view/page/itempage.dart
@@ -5,39 +5,7 @@ import '../../model/domain/item.dart';
 
 class ItemDetailsPage extends StatefulWidget {
   @override
-  _ItemDetailsPageState createState() => _ItemDetailsPageState();
-}
-
-class ProductDetailsScreen extends StatelessWidget {
-  final Map<String, dynamic> product;
-
-  ProductDetailsScreen({required this.product});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Detalhes do Produto'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              product['nome'], // substitua 'nome' pelo nome do campo que contém o nome do produto
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16),
-            Text(
-              product['descricao'], // substitua 'descricao' pelo nome do campo que contém a descrição do produto
-              style: TextStyle(fontSize: 16),
-            ),
-            // Adicione outros widgets para exibir mais detalhes do produto
-          ],
-        ),
-      ),
-    );
-  }
+  _ItemDetailsPageState createState() => _ItemDetailsPageState(item: {});
 }
 
 class _ItemDetailsPageState extends State<ItemDetailsPage> {
@@ -62,15 +30,14 @@ class _ItemDetailsPageState extends State<ItemDetailsPage> {
               ),
             ),
             SizedBox(height: 16),
-            Text(item['old_price'],
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(fontSize: 16),
-                                  overflow: TextOverflow.ellipsis),
+            Text(
+              item['old_price'],
+              style: TextStyle(fontSize: 18),
             ),
             SizedBox(height: 32),
             ElevatedButton(
               onPressed: () {
-                _launchURL(itemLink);
+                _launchURL(item['link']);
               },
               child: Text('Comprar'),
             ),
@@ -89,19 +56,3 @@ class _ItemDetailsPageState extends State<ItemDetailsPage> {
   }
 }
 
-class AppModule extends Module {
-  @override
-  final List<Bind> binds = [];
-
-  @override
-  final List<ModularRoute> routes = [
-    ChildRoute(
-      '/',
-      child: (_, args) => ItemDetailsPage(
-        itemName: args.data['name'],
-        itemPrice: args.data['price'],
-        itemLink: args.data['link'],
-      ),
-    ),
-  ];
-}
